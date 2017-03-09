00f769769a1c67ac7519d4953b02eb0b89b775ce
Nitin Sharma <ns201295@gmail.com>
2017-03-10 00:12:19 +0530

    final commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+## The function can calculate the inverse of the matrix and then 
+## save it in the cache. The function makeCacheMatrix is the first
+## step to create a matrix and the function cacheSolve is to solve  
+## the matrix, get the inversion and save it in the cache
+
+## This function makeCacheMatrix is the first step to create a matrix
+## and no calculation has been run
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -9,7 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
     getinverse <- function() i
     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
+        
+## This function is to solve the matrix, get the inversion and 
+## save it in the cache for the future use
 cacheSolve <- function(x, ...) {
     i <- x$getinverse()
     if(!is.null(i)) {
